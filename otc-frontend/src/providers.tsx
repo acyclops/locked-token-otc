7c9ed1e1eec0d8235cc1f619ed041859726dac27
acyclops <elitechowder@gmail.com>
2026-02-09 19:40:12 -0800

    finish removing arbitrum mainnet

--- otc-frontend/src/providers.tsx
+++ otc-frontend/src/providers.tsx
@@ -5,7 +5,7 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { WagmiProvider, http } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { arbitrum, arbitrumSepolia } from 'wagmi/chains';
+import { arbitrumSepolia } from 'wagmi/chains';
 
 const config = getDefaultConfig({
   appName: 'OTC Demo',
